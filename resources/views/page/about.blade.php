bccf5226fe4acb67aae377a0eb56d2289cde2c69
muhammadrifaldi <rifald84@gmail.com>
2022-12-28 10:30:11 +0700

    about page

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -15,16 +15,59 @@
         <div class="about__info">
             <div class="about__info-container">
                 <div class="about__info-container-right">
-                    <div class="about__info-container-right-title"></div>
-                    <div class="about__info-container-right-txt"></div>
-                    <div class="about__info-container-right-btn"></div>
+                    <div class="about__info-container-right-title">What is SMKN 1 Purwosari ?</div>
+                    <div class="about__info-container-right-txt"> Ocean School is a free environmental education resource
+                        for students in grades 5-12. Our inspiring and immersive multimedia resources featuring linear and
+                        360˚ videos, VR/AR, interactive media and hands-on projects and activities are offered in English
+                        and French. Our cross-curricular content spans science, social studies, language arts and more!
+                    </div>
+                    <div class="about__info-container-right-btn btn"> Explore Your Skill</div>
                 </div>
                 <div class="about__info-container-left">
-                    <div class="about__info-container-left-txt"></div>
-                    <div class="about__info-container-left-img"></div>
+                    <div class="about__info-container-left-txt">Explore the ocean through immersive multimedia lessons
+                        created by scientists, educators, and storytellers</div>
+                    <div class="about__info-container-left-img">
+                        <img src="https://d1fe71kg83l872.cloudfront.net/v2/uploads/_539x382_fit_center-center_none/whale-tablet.png"
+                            alt="">
+                    </div>
 
                 </div>
             </div>
+        </div>
+        <div class="home__action">
+            <p class="home__action-title">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, neque?
+            </p>
+            <div class="home__action-wrapper">
+                <div class="home__action-item">
+                    <div class="home__action-item-title">
+                        <p>Keunggulan</p>
+                        <div class="home__action-item-title-btn">
+                            <ion-icon name="arrow-forward-outline"></ion-icon>
+                        </div>
+                    </div>
+                    <img class="home__action-item-img" src="{{ asset('./img/ilus1.svg') }}" alt="">
+                </div>
+                <div class="home__action-item">
+                    <div class="home__action-item-title">
+                        <p>Fasilitas</p>
+                        <div class="home__action-item-title-btn">
+                            <ion-icon name="arrow-forward-outline"></ion-icon>
+                        </div>
+                    </div>
+                    <img class="home__action-item-img" src="{{ asset('./img/ilus2.svg') }}" alt="">
+                </div>
+                <div class="home__action-item">
+                    <div class="home__action-item-title">
+                        <p>Prestasi</p>
+                        <div class="home__action-item-title-btn">
+                            <ion-icon name="arrow-forward-outline"></ion-icon>
+                        </div>
+                    </div>
+                    <img class="home__action-item-img" src="{{ asset('./img/ilus3.svg') }}" alt="">
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
