cc3ccdb3e21af1199be7ba15a4fb795f028f5921
muhammadrifaldi <rifald84@gmail.com>
2022-12-29 19:12:19 +0700

    rifaldi

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -14,11 +14,54 @@
             <a href="#" class="header__list--item btn-join ">Join Us &rarr;</a>
         </div>
 
-      
+
     </div>
 </header>
 
-<header class="header-phone">
+<header class="header-popup">
+    <div class="header-popup-container">
+        <div class="header-popup-container-title">Browse Our Content</div>
+        <div class="header-popup-container-box">
+            <div class="header-popup-container-box-title">Category</div>
+            <a href="" class="header-popup-container-box-link">Home</a>
+            <a href="" class="header-popup-container-box-link">About</a>
+            <a href="" class="header-popup-container-box-link">Prestasi</a>
+            <a href="" class="header-popup-container-box-link">Keunggulan</a>
+        </div>
+        <div class="header-popup-container-box">
+            <div class="header-popup-container-box-title">Category</div>
+            <a href="" class="header-popup-container-box-link">Home</a>
+            <a href="" class="header-popup-container-box-link">About</a>
+            <a href="" class="header-popup-container-box-link">Prestasi</a>
+            <a href="" class="header-popup-container-box-link">Keunggulan</a>
+        </div>
+        <div class="header-popup-container-box">
+            <div class="header-popup-container-box-title">Category</div>
+            <a href="" class="header-popup-container-box-link">Home</a>
+            <a href="" class="header-popup-container-box-link">About</a>
+            <a href="" class="header-popup-container-box-link">Prestasi</a>
+            <a href="" class="header-popup-container-box-link">Keunggulan</a>
+        </div>
+        <div class="header-popup-container-action">
+            <div class="header-popup-container-action-box"
+                style="background: radial-gradient(74.87% 160.81% at 29.43% 109.75%,#fcacf1 0,#fcacf1 45.38%,#fe9887 74.11%,#ef4225 100%);">
+                <div class="header-popup-container-action-box-title">explore our resources</div>
+                <div class="header-popup-container-action-box-btn">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="header-popup-container-action-box"
+                style="background: radial-gradient(74.87% 160.81% at 29.43% 109.75%,#7fdddd 0,#7fdddd 45.38%,#33d2c9 74.11%,#1aa9e8 100%);">
+                <div class="header-popup-container-action-box-title">explore our resources</div>
+                <div class="header-popup-container-action-box-btn">
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+{{-- <header class="header-phone">
     <div class="header-phone-container">
         <div class="header-phone-top">
             <div class="header-phone-top__logo">
@@ -40,4 +83,4 @@
             <a href="#" class="header-phone-list-item btn-join btn">Join Us &rarr;</a>
         </div>
     </div>
-</header>
+</header> --}}
